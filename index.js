066eef90f2a180eff8a157b0a813a5aca6bb11a1
Murali <murali@appbrowser.com>
2016-09-29 17:07:11 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var expressHandlebars  = require('express-handlebars');
+var request = require('request');
+
+app.get('/', function(req, res) {
+	request('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand', function (error, response, body) {
+	  	if (!error && response.statusCode == 200) {
+	  		response_body = JSON.parse(response.body);
+	    	res.render('home',{ quoteData : response_body[0] });
+	  	}
+	})
+});
+
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.listen(5001, function () {
+	console.log("Listening on port 5001...");
+});

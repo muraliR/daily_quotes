1b07cba6eda02017790a454c4fefc030eb9e6ba4
Murali <murali@appbrowser.com>
2016-09-29 17:41:33 +0530

    dynamic port assigning

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get('/', function(req, res) {
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.listen(5001, function () {
+app.listen(process.env.PORT || 5000, function () {
 	console.log("Listening on port 5001...");
 });
